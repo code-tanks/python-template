deafd1d12cbed153639e06d7c6ccfc8ac1d00fdd
Derrick Liu <me@derrickliu.dev>
2023-01-31 11:22:36 -0800

    Update get_scripts.sh

--- get_scripts.sh
+++ get_scripts.sh
@@ -2,4 +2,4 @@
 
 mkdir scripts
 cd scripts
-curl https://codeload.github.com/code-tanks/code-tanks/tar.gz/main | tar -xz --strip=2 code-tanks-main/scripts --exclude dev
+curl https://codeload.github.com/code-tanks/code-tanks/tar.gz/main | tar -xz --strip=2 --exclude dev code-tanks-main/scripts
